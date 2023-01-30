c69b2e0405873a86df4ff49351fc5fd06aa9a1db
ktykty0722 <ktykty0722@naver.com>
2023-01-30 14:15:49 +0900

    setting(BE) : 프로젝트 생성 및 초기 세팅(#3)

---
+++ amatta_server/src/main/java/com/amatta/amatta_server/AmattaServerApplication.java
@@ -0,0 +1,13 @@
+package com.amatta.amatta_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmattaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmattaServerApplication.class, args);
+	}
+
+}
---
+++ amatta_server/src/test/java/com/amatta/amatta_server/AmattaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amatta.amatta_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmattaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
