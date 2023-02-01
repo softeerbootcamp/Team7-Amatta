9ab682b122cbd255c7d984bf7f3ca02f5edd7ae9
201724554 <pay7845@naver.com>
2023-02-01 18:43:12 +0900

    feat(BE) : EnableAspectJAutoProxy annotation 추가 (#13)

--- amatta_server/src/main/java/com/amatta/amatta_server/AmattaServerApplication.java
+++ amatta_server/src/main/java/com/amatta/amatta_server/AmattaServerApplication.java
@@ -2,8 +2,10 @@ package com.amatta.amatta_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class AmattaServerApplication {
 
 	public static void main(String[] args) {
