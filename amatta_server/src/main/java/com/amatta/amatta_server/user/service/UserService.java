d6f56b9c57539c506edc708e6f2411546378ab49
ktykty0722 <ktykty0722@naver.com>
2023-02-02 23:58:20 +0900

    feat(BE) : UserService에 로그인 메소드 추가(#10) - 이메일로 유저를 찾고, 유저가 있을시 비밀번호 비교를 통해 검증

--- amatta_server/src/main/java/com/amatta/amatta_server/user/service/UserService.java
+++ amatta_server/src/main/java/com/amatta/amatta_server/user/service/UserService.java
@@ -2,7 +2,10 @@ package com.amatta.amatta_server.user.service;
 
 import com.amatta.amatta_server.user.dto.UserJoinReq;
 import com.amatta.amatta_server.user.dto.UserJoinRes;
+import com.amatta.amatta_server.user.dto.UserLoginReq;
+import com.amatta.amatta_server.user.model.Users;
 import com.amatta.amatta_server.user.repository.UserRepository;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -17,13 +20,11 @@ public class UserService {
     }
 
     public UserJoinRes signUp(UserJoinReq userJoinReq) {
-
-        userRepository.addUser(userJoinReq.getEmail(), userJoinReq.getPassword(), userJoinReq.getName(), userJoinReq.getPhoneNumber());
+        userRepository.addUser(userJoinReq.getEmail(), BCrypt.hashpw(userJoinReq.getPassword(), BCrypt.gensalt()), userJoinReq.getName(), userJoinReq.getPhoneNumber());
         UserJoinRes userJoinRes = new UserJoinRes();
         userJoinRes.setSuccess(true);
         return userJoinRes;
     }
-
 
     public boolean checkEmailDuplicated(String email) {
         return Objects.nonNull(userRepository.findByEmail(email));
@@ -31,5 +32,13 @@ public class UserService {
 
     public boolean checkPhoneNumDuplicated(String phoneNumber) {
         return Objects.nonNull(userRepository.findByPhoneNum(phoneNumber));
+    }
+
+    public Users login(UserLoginReq userLoginReq) {
+        Users users = userRepository.findByEmail(userLoginReq.getEmail());
+        if(Objects.nonNull(users) && BCrypt.checkpw(userLoginReq.getPassword(), users.getPassword())) {
+            return users;
+        }
+        return null;
     }
 }
