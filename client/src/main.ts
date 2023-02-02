3b64385673c6b88316c4313e85e7a698cbc4c8bf
SUMMERLOVE7 <wkdalswl77@naver.com>
2023-02-02 15:32:31 +0900

    feat(FE) : 헤더 구현완료(#14)

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,11 @@
 import './styles/main.scss';
 import { setScreenSize } from './utils';
+import { showSignup } from './views/signupView';
 
 window.addEventListener('resize', setScreenSize);
+
+const loginBtn = document.querySelector('.login-button');
+if (loginBtn instanceof HTMLElement) {
+  //   loginBtn.addEventListener('click', showHeader);
+  loginBtn.addEventListener('click', showSignup);
+}
