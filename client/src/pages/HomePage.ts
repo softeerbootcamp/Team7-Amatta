e49e4706a9ee5bf161b2a4a298841cebe9145c61
sunjh96 <sunjh96@gmail.com>
2023-02-03 04:02:00 +0900

    feat(FE) : SPA Router 구현 (#18)
    1. 뷰 리팩토링
    2. 라우터 구현

---
+++ client/src/pages/HomePage.ts
@@ -0,0 +1,22 @@
+import { SERVER_URL } from '@/constants/constant';
+import { generateElement } from '@/utils';
+
+const HomePage = () => {
+  const logoUrl = `${SERVER_URL.IMG}logo/logo-white.png`;
+
+  const homeTemplate = generateElement(`
+    <main class="home-main">
+      <section class="home-section">
+        <img class="logo-white" src="${logoUrl}" alt="amatta-logo" />
+        <section class="home-button-section">
+          <a class="login-button" href='/'>로그인</a>
+          <a class="signup-button" href='/register'>회원가입</a>
+        </section>
+      </section>
+    </main>
+  `);
+
+  return homeTemplate;
+};
+
+export default HomePage;
--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,1 +1,2 @@
 export { default as setScreenSize } from './screenSize';
+export { default as generateElement } from './templateGenerator';
