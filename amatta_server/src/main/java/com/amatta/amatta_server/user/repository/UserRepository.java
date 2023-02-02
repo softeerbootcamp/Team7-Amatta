7ca32aff6d07808e8def38e901c8a8310aed22f9
ktykty0722 <ktykty0722@naver.com>
2023-02-02 15:36:31 +0900

    feat(BE) : 이메일, 전화번호로 User 검색하는 메소드 추가(#23)

--- amatta_server/src/main/java/com/amatta/amatta_server/user/repository/UserRepository.java
+++ amatta_server/src/main/java/com/amatta/amatta_server/user/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.amatta.amatta_server.user.repository;
 
 import com.amatta.amatta_server.user.dto.UserJoinReq;
+import com.amatta.amatta_server.user.model.Users;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,9 +17,27 @@ public class UserRepository {
 
     @Value("${key}")
     private String key;
+
     @Autowired
     public UserRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Users findByEmail(String email) {
+        try {
+            return jdbcTemplate.queryForObject("select * from users where email = ?", userRowMapper(), email);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    public Users findByPhoneNum(String phoneNumber) {
+        try {
+            return jdbcTemplate.queryForObject(
+                    "select * from users where phoneNumber = ?", userRowMapper(), phoneNumber);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     public void addUser(UserJoinReq userJoinReq) {
@@ -27,5 +48,16 @@ public class UserRepository {
                 userJoinReq.getName(),
                 userJoinReq.getPhoneNumber()
         );
+    }
+
+    private RowMapper<Users> userRowMapper() {
+        return (resultSet, rowNum) -> {
+            long userId = (resultSet.getLong("id"));
+            String userEmail = (resultSet.getString("email"));
+            String userPassword = (resultSet.getString("password"));
+            String userName = (resultSet.getString("name"));
+            String userPhoneNum = (resultSet.getString("phoneNumber"));
+            return new Users(userId, userEmail, userPassword, userName, userPhoneNum);
+        };
     }
 }
