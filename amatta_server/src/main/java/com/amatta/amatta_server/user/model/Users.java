9f0f62e9d272fda9d66a1a000309564b7056f1a4
ktykty0722 <ktykty0722@naver.com>
2023-02-02 15:39:44 +0900

    feat(BE) : Users 생성자 추가(#23)

--- amatta_server/src/main/java/com/amatta/amatta_server/user/model/Users.java
+++ amatta_server/src/main/java/com/amatta/amatta_server/user/model/Users.java
@@ -7,6 +7,14 @@ public class Users {
     private String name;
     private String phoneNumber;
 
+    public Users(long id, String email, String password, String name, String phoneNumber) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
     public long getId() {
         return id;
     }
