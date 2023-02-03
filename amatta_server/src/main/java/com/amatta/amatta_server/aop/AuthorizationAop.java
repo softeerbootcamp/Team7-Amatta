a8b4a720f614ce8c21d7f0337b790ae619d36bb7
201724554 <pay7845@naver.com>
2023-02-03 10:16:56 +0900

    feat(BE) : 간단한 쿠키 검증 메소드 구현 (#28)

--- amatta_server/src/main/java/com/amatta/amatta_server/aop/AuthorizationAop.java
+++ amatta_server/src/main/java/com/amatta/amatta_server/aop/AuthorizationAop.java
@@ -1,7 +1,6 @@
 package com.amatta.amatta_server.aop;
 
 import com.amatta.amatta_server.exception.NotAuthenticatedException;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -10,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Objects;
 
@@ -21,12 +21,24 @@ public class AuthorizationAop {
     @Before("within(@com.amatta.amatta_server.aop.ClassRequiresAuth *)")
     public void authorizeClass() throws NotAuthenticatedException, NullPointerException {
         logger.info("AuthorizationAop.authorize");
+
         HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+        Cookie[] cookies = request.getCookies();
+
+        if(cookies == null || cookies.length == 0) {
+            throw new NotAuthenticatedException();
+        }
     }
 
     @Before("@annotation(com.amatta.amatta_server.aop.MethodRequiresAuth)")
     public void authorizeMethod() throws NotAuthenticatedException, NullPointerException {
         logger.info("AuthorizationAop.authorize");
+
         HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+        Cookie[] cookies = request.getCookies();
+
+        if(cookies == null || cookies.length == 0) {
+            throw new NotAuthenticatedException();
+        }
     }
 }
