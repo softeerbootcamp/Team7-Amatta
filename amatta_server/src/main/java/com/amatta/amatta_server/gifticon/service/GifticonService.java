1d9c670a0d0a3a672c7c490c1aa67d32618af2f6
201724554 <pay7845@naver.com>
2023-02-04 18:05:43 +0900

    feat(BE) : extractGifticonText - POST /gift/text를 핸들하는 메소드 (#31)

--- amatta_server/src/main/java/com/amatta/amatta_server/gifticon/service/GifticonService.java
+++ amatta_server/src/main/java/com/amatta/amatta_server/gifticon/service/GifticonService.java
@@ -1,14 +1,38 @@
 package com.amatta.amatta_server.gifticon.service;
 
 import com.amatta.amatta_server.aop.ClassRequiresAuth;
+import com.amatta.amatta_server.gifticon.dto.GifticonImageDto;
 import com.amatta.amatta_server.gifticon.model.Gifticon;
+import com.amatta.amatta_server.gifticon.util.RequestGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
 @ClassRequiresAuth
 public class GifticonService {
+    private final RequestGenerator requestGenerator;
+
+    @Autowired
+    public GifticonService(RequestGenerator requestGenerator) {
+        this.requestGenerator = requestGenerator;
+    }
+
+    public ResponseEntity<String> extractGifticonText(GifticonImageDto dto) {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(
+                requestGenerator.getUrl(),
+                HttpMethod.POST,
+                requestGenerator.generate(dto),
+                String.class
+        );
+        return response;
+    }
+
     public List<Gifticon> findGifticonList() {
         return null;
     }
