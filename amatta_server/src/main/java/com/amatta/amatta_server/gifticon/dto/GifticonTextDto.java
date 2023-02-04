b7c055b03a4793a1e3b6f67c13237cc39fd78868
201724554 <pay7845@naver.com>
2023-02-04 18:39:35 +0900

    feat(BE) : POST /gift에 사용될, 기프티콘 텍스트를 담는 dto (#31)

---
+++ amatta_server/src/main/java/com/amatta/amatta_server/gifticon/dto/GifticonTextDto.java
@@ -0,0 +1,11 @@
+package com.amatta.amatta_server.gifticon.dto;
+
+import java.util.List;
+
+public class GifticonTextDto {
+    private List<String> texts;
+
+    public List<String> getTexts() {
+        return texts;
+    }
+}
