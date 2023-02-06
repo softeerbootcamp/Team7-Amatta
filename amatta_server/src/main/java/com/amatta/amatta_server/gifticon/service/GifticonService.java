fcd3c63007f5ff810f24ba43f3fee600e973f263
201724554 <pay7845@naver.com>
2023-02-06 18:08:29 +0900

    feat(BE) : 텍스트 정보로 기프티콘 객체를 리턴하는 mapTextToGifticon 메소드 추가 (#50)

--- amatta_server/src/main/java/com/amatta/amatta_server/gifticon/service/GifticonService.java
+++ amatta_server/src/main/java/com/amatta/amatta_server/gifticon/service/GifticonService.java
@@ -2,7 +2,10 @@ package com.amatta.amatta_server.gifticon.service;
 
 import com.amatta.amatta_server.aop.ClassRequiresAuth;
 import com.amatta.amatta_server.gifticon.dto.GifticonImageDto;
+import com.amatta.amatta_server.gifticon.dto.GifticonTextDto;
 import com.amatta.amatta_server.gifticon.model.Gifticon;
+import com.amatta.amatta_server.gifticon.util.GifticonMapper;
+import com.amatta.amatta_server.gifticon.util.GifticonMapperFactory;
 import com.amatta.amatta_server.gifticon.util.RequestGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
@@ -11,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @ClassRequiresAuth
@@ -24,13 +28,17 @@ public class GifticonService {
 
     public ResponseEntity<String> extractGifticonText(GifticonImageDto dto) {
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(
+        return restTemplate.exchange(
                 requestGenerator.getUrl(),
                 HttpMethod.POST,
                 requestGenerator.generate(dto),
                 String.class
         );
-        return response;
+    }
+
+    public Gifticon mapTextToGifticon(GifticonTextDto dto) throws NullPointerException, IndexOutOfBoundsException {
+        GifticonMapper mapper = Objects.requireNonNull(GifticonMapperFactory.getGifticonMapper(dto.getTexts()));
+        return mapper.map(dto.getTexts());
     }
 
     public List<Gifticon> findGifticonList() {
