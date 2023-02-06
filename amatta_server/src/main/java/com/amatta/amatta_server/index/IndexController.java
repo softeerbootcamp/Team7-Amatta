3c653ca0ac6f2aedf84b52082de7b61977f2b836
ktykty0722 <ktykty0722@naver.com>
2023-02-06 17:38:34 +0900

    feat(BE) : crossOrigin 어노테이션 추가

--- amatta_server/src/main/java/com/amatta/amatta_server/index/IndexController.java
+++ amatta_server/src/main/java/com/amatta/amatta_server/index/IndexController.java
@@ -3,12 +3,14 @@ package com.amatta.amatta_server.index;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
 @RestController
+@CrossOrigin
 public class IndexController {
     @GetMapping("/redirect")
     public ResponseEntity<?> redirect() {
