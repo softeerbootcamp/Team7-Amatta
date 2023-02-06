1b7a40860bfead31bd936034c51b5eb32056a40b
ktykty0722 <ktykty0722@naver.com>
2023-02-06 16:16:07 +0900

    test(BE) : 유저 관련 기능 테스트 코드 작성(#46)

--- amatta_server/src/test/java/com/amatta/amatta_server/user/UserControllerTest.java
+++ amatta_server/src/test/java/com/amatta/amatta_server/user/UserControllerTest.java
@@ -3,6 +3,10 @@ package com.amatta.amatta_server.user;
 import com.amatta.amatta_server.user.controller.UserController;
 import com.amatta.amatta_server.user.dto.UserJoinReq;
 import com.amatta.amatta_server.user.dto.UserJoinRes;
+import com.amatta.amatta_server.user.dto.UserLoginReq;
+import com.amatta.amatta_server.user.dto.UserLoginRes;
+import com.amatta.amatta_server.user.model.Users;
+import com.amatta.amatta_server.user.service.UserService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,14 +21,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class UserControllerTest {
 
     @Autowired
     UserController userController;
 
+    @Autowired
+    UserService userService;
+
     @Test
     @DisplayName("정상 회원가입 테스트")
-    @Transactional
     void join1() {
         UserJoinReq userJoinReq = new UserJoinReq(
                 "test@test.com",
@@ -40,7 +47,6 @@ public class UserControllerTest {
 
     @Test
     @DisplayName("회원가입 테스트 - 중복된 이메일 입력")
-    @Transactional
     void join2() {
         UserJoinReq userJoinReq1 = new UserJoinReq(
                 "test@test.com",
@@ -63,10 +69,9 @@ public class UserControllerTest {
 
     @Test
     @DisplayName("회원가입 테스트 - 중복된 전화번호 입력")
-    @Transactional
     void join3() {
         UserJoinReq userJoinReq1 = new UserJoinReq(
-                "test1@test.com",
+                "test12@test.com",
                 "testPassword",
                 "test",
                 "010-0000-0000"
@@ -83,6 +88,51 @@ public class UserControllerTest {
             userController.join(userJoinReq2);
         });
     }
+
+    @Test
+    @DisplayName("정상 로그인 테스트")
+    void login1() {
+        String email = "test@test.com";
+        String password = "testPassword";
+        String name = "test";
+        String phoneNumber = "010-0000-0000";
+        UserJoinReq userJoinReq1 = new UserJoinReq(
+                email,
+                password,
+                name,
+                phoneNumber
+        );
+        userController.join(userJoinReq1);
+
+        UserLoginReq userLoginReq = new UserLoginReq(email, password);
+
+        Users user = userService.login(userLoginReq);
+        assertEquals(user.getEmail(), email);
+        assertEquals(user.getName(), name);
+        assertEquals(user.getPhoneNumber(), phoneNumber);
+    }
+
+    @Test
+    @DisplayName("로그인 실패 테스트")
+    void login2() {
+        String email = "test@test.com";
+        String password = "testPassword";
+        String name = "test";
+        String phoneNumber = "010-0000-0000";
+        UserJoinReq userJoinReq1 = new UserJoinReq(
+                email,
+                password,
+                name,
+                phoneNumber
+        );
+        userController.join(userJoinReq1);
+
+        UserLoginReq userLoginReq = new UserLoginReq(email, password+1);
+
+        Users user = userService.login(userLoginReq);
+        assertNull(user);
+    }
+
 
 
 }
