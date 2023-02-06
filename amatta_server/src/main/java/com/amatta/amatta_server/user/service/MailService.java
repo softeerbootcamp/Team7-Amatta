18abf9940e6c58b470355fa2575c0797cda0c83f
ktykty0722 <ktykty0722@naver.com>
2023-02-06 14:16:35 +0900

    feat(BE) : MailService 클래스 추가(#37) - 인증번호 보내주는 메소드 추가

---
+++ amatta_server/src/main/java/com/amatta/amatta_server/user/service/MailService.java
@@ -0,0 +1,55 @@
+package com.amatta.amatta_server.user.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.util.UUID;
+
+@Service
+public class MailService {
+
+    private final JavaMailSender emailSender;
+    private String authNum;
+
+    @Autowired
+    public MailService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    public String sendEmail(String toEmail) {
+        MimeMessage emailForm = createEmailForm(toEmail);
+        emailSender.send(emailForm);
+        return authNum;
+    }
+    private MimeMessage createEmailForm(String email){
+        try {
+            String title = "[Amatta] 회원가입 인증 번호";
+            authNum = UUID.randomUUID().toString().substring(0, 6);
+            String text =
+                    "<div style=\"margin:100px; font-family: verdana\">\n" +
+                            "    <h1> 안녕하세요 아마따입니다.</h1><br>\n" +
+                            "    <p> 아래 코드를 회원가입 창으로 돌아가 입력해주세요.</p><br>\n" +
+                            "    <div align=\"center\" style=\"border:1px solid #92B8B1;\">\n" +
+                            "        <h3 style=\"color:#212121\"> 회원가입 인증 코드 입니다. </h3>\n" +
+                            "        <div style=\"font-size:130%\">" + authNum + "</div>\n" +
+                            "        <br>\n" +
+                            "    </div>\n" +
+                            "    <br/>\n" +
+                            "</div>";
+
+            MimeMessage message = emailSender.createMimeMessage();
+
+            message.addRecipients(MimeMessage.RecipientType.TO, email); //보낼 이메일 설정
+            message.setSubject(title); //제목 설정
+            message.setText(text, "utf-8", "html");
+
+            return message;
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+}
