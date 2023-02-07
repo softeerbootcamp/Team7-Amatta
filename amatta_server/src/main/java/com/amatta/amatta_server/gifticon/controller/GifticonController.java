f1b20273ea07dbc6eb88e3afe96c03f6c5ba916a
201724554 <pay7845@naver.com>
2023-02-07 18:49:17 +0900

    feat(BE) : POST /gifticon API 구현 (#60)

--- amatta_server/src/main/java/com/amatta/amatta_server/gifticon/controller/GifticonController.java
+++ amatta_server/src/main/java/com/amatta/amatta_server/gifticon/controller/GifticonController.java
@@ -1,6 +1,8 @@
 package com.amatta.amatta_server.gifticon.controller;
 
+import com.amatta.amatta_server.exception.DuplicateGifticonException;
 import com.amatta.amatta_server.exception.NotAuthenticatedException;
+import com.amatta.amatta_server.gifticon.dto.GifticonDto;
 import com.amatta.amatta_server.gifticon.dto.GifticonImageDto;
 import com.amatta.amatta_server.gifticon.dto.GifticonTextDto;
 import com.amatta.amatta_server.gifticon.model.Gifticon;
@@ -8,7 +10,10 @@ import com.amatta.amatta_server.gifticon.service.GifticonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @CrossOrigin
@@ -33,11 +38,9 @@ public class GifticonController {
     }
 
     @PostMapping("")
-    public ResponseEntity<?> gifticonAdd(@RequestBody GifticonTextDto dto) {
-        //System.out.println(dto.getTexts());
-        return null;
-        //List<Gifticon> gifticonList = gifticonService.findGifticonList();
-        //return new ResponseEntity<>(gifticonList, HttpStatus.OK);
+    public ResponseEntity<?> gifticonAdd(@Valid @RequestBody GifticonDto dto) {
+        gifticonService.addGifticon(dto);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @ExceptionHandler(NotAuthenticatedException.class)
@@ -53,5 +56,15 @@ public class GifticonController {
     @ExceptionHandler(IndexOutOfBoundsException.class)
     public ResponseEntity<?> indexOutOfBoundExceptionHandler() {
         return new ResponseEntity<>("잘못된 형식의 이미지", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DuplicateGifticonException.class)
+    public ResponseEntity<?> duplicateGifticonExceptionHandler() {
+        return new ResponseEntity<>("중복되는 바코드를 가진 기프티콘이 존재", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> methodArgumentNotValidExceptionHandler() {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
