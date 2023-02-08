9270644ccced75531e75be1443f403d5b74cbc5d
ktykty0722 <ktykty0722@naver.com>
2023-02-08 17:59:58 +0900

    feat(BE) : 임시비밀번호를 보내주는 메소드 추가(#75)

--- amatta_server/src/main/java/com/amatta/amatta_server/user/service/MailService.java
+++ amatta_server/src/main/java/com/amatta/amatta_server/user/service/MailService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.util.UUID;
 
@@ -19,12 +18,19 @@ public class MailService {
         this.emailSender = emailSender;
     }
 
-    public String sendEmail(String toEmail) {
-        MimeMessage emailForm = createEmailForm(toEmail);
+    public String sendJoinEmail(String toEmail) {
+        MimeMessage emailForm = createJoinEmailForm(toEmail);
         emailSender.send(emailForm);
         return authNum;
     }
-    private MimeMessage createEmailForm(String email){
+
+    public String sendPasswordFindEmail(String toEmail) {
+        MimeMessage emailForm = createPasswordFindEmailForm(toEmail);
+        emailSender.send(emailForm);
+        return authNum;
+    }
+
+    private MimeMessage createJoinEmailForm(String email) {
         try {
             String title = "[Amatta] 회원가입 인증 번호";
             authNum = UUID.randomUUID().toString().substring(0, 6);
@@ -34,6 +40,34 @@ public class MailService {
                             "    <p> 아래 코드를 회원가입 창으로 돌아가 입력해주세요.</p><br>\n" +
                             "    <div align=\"center\" style=\"border:1px solid #92B8B1;\">\n" +
                             "        <h3 style=\"color:#212121\"> 회원가입 인증 코드 입니다. </h3>\n" +
+                            "        <div style=\"font-size:130%\">" + authNum + "</div>\n" +
+                            "        <br>\n" +
+                            "    </div>\n" +
+                            "    <br/>\n" +
+                            "</div>";
+
+            MimeMessage message = emailSender.createMimeMessage();
+
+            message.addRecipients(MimeMessage.RecipientType.TO, email); //보낼 이메일 설정
+            message.setSubject(title); //제목 설정
+            message.setText(text, "utf-8", "html");
+
+            return message;
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private MimeMessage createPasswordFindEmailForm(String email) {
+        try {
+            String title = "[Amatta] 회원가입 인증 번호";
+            authNum = UUID.randomUUID().toString().substring(0, 10);
+            String text =
+                    "<div style=\"margin:100px; font-family: verdana\">\n" +
+                            "    <h1> 안녕하세요 아마따입니다.</h1><br>\n" +
+                            "    <p> 아래 임시 비밀번호로 로그인 하신 후 비밀번호를 변경해주세요.</p><br>\n" +
+                            "    <div align=\"center\" style=\"border:1px solid #92B8B1;\">\n" +
+                            "        <h3 style=\"color:#212121\"> 임시 비밀번호 입니다. </h3>\n" +
                             "        <div style=\"font-size:130%\">" + authNum + "</div>\n" +
                             "        <br>\n" +
                             "    </div>\n" +
