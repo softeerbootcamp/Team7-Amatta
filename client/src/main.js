439da0b766e30023b30836c1e3ae2449a553a474
sunjh96 <sunjh96@gmail.com>
2023-02-08 21:49:28 +0900

    feature(FE) : 포스팅페이지 UI 개발(#79)

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,13 @@
 import '@/styles/style.scss';
 import { setScreenSize } from '@/utils';
-import { navigateHome, navigateAuth, navigateMain } from '@/pages';
+import { navigateHome, navigateAuth, navigateMain, navigatePost } from '@/pages';
 import { routes, navigate } from '@/core/router';
 
 routes.push(
   { path: '/', component: navigateHome },
   { path: '/register', component: navigateAuth },
   { path: '/card', component: navigateMain },
+  { path: '/post', component: navigatePost },
 );
 const path = window.location.pathname;
 
