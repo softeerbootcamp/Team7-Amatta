ff26f0e69cdf3f3db82f21260e54623c839ecb27
SUMMERLOVE7 <wkdalswl77@naver.com>
2023-02-08 15:27:09 +0900

    feat(FE) : 시간에 따라 카드 자동 슬라이딩

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -12,22 +12,19 @@ const plusIconUrl = `${SERVER_URL.IMG}icon/plus.svg`;
 
 const cards = [
   {
-    image:
-      'https://amatta-icons.s3.ap-northeast-2.amazonaws.com/icon/gifticonSample.jpeg',
+    image: 'https://amatta-icons.s3.ap-northeast-2.amazonaws.com/icon/gifticonSample.jpeg',
     shopName: 'twosome place',
     itemName: 'Americano & Tiramisu',
     dateOfUse: '2023.07.07 까지',
   },
   {
-    image:
-      'https://amatta-icons.s3.ap-northeast-2.amazonaws.com/icon/gifticonSample.jpeg',
+    image: 'https://amatta-icons.s3.ap-northeast-2.amazonaws.com/icon/gifticonSample.jpeg',
     shopName: 'starbucks',
     itemName: 'Latte',
     dateOfUse: '2023.07.07 까지',
   },
   {
-    image:
-      'https://amatta-icons.s3.ap-northeast-2.amazonaws.com/icon/gifticonSample.jpeg',
+    image: 'https://amatta-icons.s3.ap-northeast-2.amazonaws.com/icon/gifticonSample.jpeg',
     shopName: 'The venti',
     itemName: 'Vanilla Latte',
     dateOfUse: '2023.07.07 까지',
@@ -60,11 +57,29 @@ MainPage.temp = `
   `;
 
 // prettier-ignore
-const navigateMain = () =>
+MainPage.render =
+  new Promise((resolve) =>
     _.go(
-      MainPage.temp, 
+      MainPage.temp,
       $.el,
-      $.replace($.qs('#root')));
+      $.replace($.qs('#root')),
+      _ => resolve($.qsa('[data-link]'))));
+
+// prettier-ignore
+const navigateMain = () => 
+  new Promise (async (resolve) => 
+    _.go(
+      await MainPage.render,
+      resolve,
+      slider));
+
+// // prettier-ignore
+// const navigateMain = () =>
+//     _.go(
+//       MainPage.temp,
+//       $.el,
+//       $.replace($.qs('#root')))
+// slider());
 
 // slider();
 
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,5 @@
 export { default as navigateHome } from './HomePage';
 export { default as navigateAuth } from './AuthPage';
 export { default as navigateMain } from './MainPage';
-export { default as SlidePage } from './SlidePage';
 
 export { default as NotFound } from './NotFoundPage';
