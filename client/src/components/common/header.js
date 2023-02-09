2860a0751c0ba47b936240b9b80699613410cade
sunjh96 <sunjh96@gmail.com>
2023-02-09 13:31:39 +0900

    feature(FE) : header 컴포넌트 구조 변경(#79)

--- client/src/components/common/header.js
+++ client/src/components/common/header.js
@@ -1,20 +1,39 @@
 import SERVER_URL from '@/constants/constant';
+import { _ } from '@/utils/customFx';
+import { $ } from '@/utils';
 
-const logoUrl = `${SERVER_URL.IMG}logo/logo-white.png`;
-const iconUrl = `${SERVER_URL.IMG}icon/search.svg`;
+const header = (props) => {
+  const { color, label, target } = props;
+  const logoUrl = `${SERVER_URL.IMG}logo/logo-white.png`;
+  const iconUrl = `${SERVER_URL.IMG}icon/search.svg`;
 
-const headerTemplate = `
-    <article class = 'header-article'>
-        <img class='small-logo-white' src='${logoUrl}' alt='amatta-small-logo'/>
-        <section class='header-button-section'>
-          <img class="search-button" src = '${iconUrl}' alt='search-button' />
-          <section class="trigger">
-            <span></span>
-            <span></span>
-            <span></span>
+  const headerTemp = `
+      <header class = 'header-main'>
+          <img class='small-logo-white' src='${logoUrl}' alt='amatta-small-logo'/>
+          <section class='header-button-section'>
+            <img class="search-button" src = '${iconUrl}' alt='search-button' />
+            <section class="trigger">
+              <span></span>
+              <span></span>
+              <span></span>
+            </section>
           </section>
-        </section>
-    </article>
-  `;
+      </header>
+    `;
 
-export default headerTemplate;
+  // prettier-ignore
+  const render = () =>
+    _.go(
+      headerTemp,
+      $.el,
+      $.prepend($.qs(`${target}`)));
+
+  // prettier-ignore
+  const appendHeader = () => 
+    _.go(
+      render());
+
+  return appendHeader;
+};
+
+export default header;
--- client/src/components/common/index.js
+++ client/src/components/common/index.js
@@ -1,3 +1,3 @@
-export { default as headerTemplate } from './header';
+export { default as header } from './header';
 export { default as inputForm } from './inputForm';
 export { default as cardDetail } from './cardDetail';
