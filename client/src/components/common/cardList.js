b544e8a05f53ac5df5f3054e093a69e9562d662f
SUMMERLOVE7 <wkdalswl77@naver.com>
2023-02-12 23:25:45 +0900

    feat(FE) : main 페이지 리스트 템플릿 작성(#108)

---
+++ client/src/components/common/cardList.js
@@ -0,0 +1,33 @@
+import { SERVER_URL } from '@/constants/constant.js';
+
+const cardList = (props) => {
+  const { image, shopName, itemName, dateOfUse, required } = props;
+
+  const moreIconUrl = `${SERVER_URL.IMG}icon/dots.svg`;
+
+  const cardListTemplate = `
+    <section class='one-list-section'>
+      <img class='card-list-image' src="${image}" alt='card-list-image' />
+      <section class='card-list-text'>
+        <img class='more-dots-button'src='${moreIconUrl}' alt='more-dots-button' />
+        <div class='list-d-day'> ${dateOfUse} </div>
+        <div class='list-shop-name'> ${shopName} </div>
+        <div class='list-item-name'> ${itemName} </div>
+        <div class='list-date-of-use'> ${dateOfUse} </div>
+      </section>
+    </section>
+    `;
+
+  // prettier-ignore
+  // const render = () =>
+  //   new Promise(resolve =>
+  //     _.go(
+  //     cardListTemplate,
+  //     $.el,
+  //     $.replace($.qs('.cards-section')),
+  //   resolve));
+
+  return cardListTemplate;
+};
+
+export default cardList;
--- client/src/components/common/index.js
+++ client/src/components/common/index.js
@@ -1,3 +1,4 @@
 export { default as header } from './header';
 export { default as inputForm } from './inputForm';
 export { default as cardDetail } from './cardDetail';
+export { default as cardList } from './cardList';
