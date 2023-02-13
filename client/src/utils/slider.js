42e7ecf881692a4c9c946d345031e21022ac8359
SUMMERLOVE7 <wkdalswl77@naver.com>
2023-02-13 10:44:51 +0900

    refactor(FE) : 함수형으로 리팩토링(#73)

--- client/src/utils/slider.js
+++ client/src/utils/slider.js
@@ -1,7 +1,7 @@
 import { $ } from '@/utils';
 import { _ } from '@/utils/customFx';
-
 const slider = () => {
+  //slider.js 리팩토링하려다 실패한 부분 => 2/13 성공!!
   const mainArticle = $.qs('.main-card-article');
   const slide = $.qs('.cards-section');
   const slideWidth = mainArticle.clientWidth;
@@ -10,113 +10,99 @@ const slider = () => {
   let currentSlide = 1;
   let startPoint = 0;
   let endPoint = 0;
-
-  slide.style.width = `${mainArticle.clientWidth * slideItems.length}px`;
-
-  [...slideItems].forEach((card) => {
-    card.style.width = `${mainArticle.clientWidth}px`;
-  });
-
-  let currSlide = 1;
-
-  const pagination = $.qs('.card-pagination');
-
-  const paginationTemp = `<li>•</li>`;
-  const activePaginationTemp = `<li class="active">•</li>`;
-
+  const setWidth = () => {
+    slide.style.width = `${mainArticle.clientWidth * slideItems.length}px`;
+    [...slideItems].forEach((card) => {
+      card.style.width = `${mainArticle.clientWidth}px`;
+    });
+  };
   //prettier-ignore
-  const setUnactive = () =>
+  const addActiveClass = (template, element) =>
     _.go(
-      paginationTemp,
+      template,
       $.el,
-      $.prepend(pagination));
-
-  //prettier-ignore
-  const setActive = () =>
-    _.go(
-      activePaginationTemp,
-      $.el,
-      $.prepend(pagination));
-
-  for (let i = 0; i < maxSlide; i++) {
-    if (i === maxSlide - 1) setActive();
-    else setUnactive();
-  }
-
+      $.prepend(element));
+  const setPagination = () => {
+    const pagination = $.qs('.card-pagination');
+    const paginationTemp = `<li>•</li>`;
+    const activePaginationTemp = `<li class="active">•</li>`;
+    for (let i = 0; i < maxSlide; i++) {
+      if (i === maxSlide - 1) addActiveClass(activePaginationTemp, pagination);
+      else addActiveClass(paginationTemp, pagination);
+    }
+  };
   const paginationItems = $.qsa('.card-pagination > li');
-
+  const changeActive = (targets) => {
+    targets.forEach((target) => target.classList.remove('active'));
+    targets[currentSlide - 1].classList.add('active');
+    return targets;
+  };
+  const setOffset = (targets) => {
+    const offset = slideWidth * (currentSlide - 1);
+    targets.forEach((target) => {
+      target.setAttribute('style', `left: ${-offset}px`);
+    });
+  };
+  const setSlideAttribute = () => {
+    setOffset(slideItems);
+    //changeActive(paginationItems);
+  };
   const nextMove = () => {
-    currSlide++;
-
-    if (currSlide <= maxSlide) {
-      const offset = slideWidth * (currSlide - 1);
-      slideItems.forEach((i) => {
-        i.setAttribute('style', `left: ${-offset}px`);
-      });
-      paginationItems.forEach((i) => i.classList.remove('active'));
-      paginationItems[currSlide - 1].classList.add('active');
+    currentSlide++;
+    if (currentSlide <= maxSlide) {
+      setSlideAttribute();
     } else {
-      currSlide--;
+      currentSlide--;
     }
   };
   const prevMove = () => {
-    currSlide--;
-
-    if (currSlide > 0) {
-      const offset = slideWidth * (currSlide - 1);
-      slideItems.forEach((i) => {
-        i.setAttribute('style', `left: ${-offset}px`);
-      });
-
-      paginationItems.forEach((i) => i.classList.remove('active'));
-      paginationItems[currSlide - 1].classList.add('active');
+    currentSlide--;
+    if (currentSlide > 0) {
+      setSlideAttribute();
     } else {
-      currSlide++;
+      currentSlide++;
     }
   };
-
-  // window.addEventListener('resize', () => {
-  //   slideWidth = slide.clientWidth;
-  // });
-
-  for (let i = 0; i < maxSlide; i++) {
-    paginationItems[i].addEventListener('click', () => {
-      currSlide = i + 1;
-
-      const offset = slideWidth * (currSlide - 1);
-
-      slideItems.forEach((i) => {
-        i.setAttribute('style', `left: ${-offset}px`);
+  const clickPagination = (targets) => {
+    for (let i = 0; i < maxSlide; i++) {
+      targets[i].addEventListener('click', () => {
+        currentSlide = i + 1;
+        setSlideAttribute();
       });
-      paginationItems.forEach((i) => i.classList.remove('active'));
-      paginationItems[currSlide - 1].classList.add('active');
-    });
-  }
-
-  slide.addEventListener('mousedown', (e) => {
-    startPoint = e.pageX;
-  });
-
-  slide.addEventListener('mouseup', (e) => {
-    endPoint = e.pageX;
-    if (startPoint < endPoint) {
-      prevMove();
-    } else if (startPoint > endPoint) {
-      nextMove();
     }
-  });
-
-  slide.addEventListener('touchstart', (e) => {
-    startPoint = e.touches[0].pageX;
-  });
-  slide.addEventListener('touchend', (e) => {
+  };
+  // const mouseDownEvent = () => (startPoint = e.pageX);
+  // const mouseUpEvent = () => {
+  //   endPoint = e.pageX;
+  //   if (startPoint < endPoint) {
+  //     prevMove();
+  //   } else if (startPoint > endPoint) {
+  //     nextMove();
+  //   }
+  // };
+  const touchStartEvent = (target) => (startPoint = target.touches[0].pageX);
+  const touchEndEvent = (e) => {
     endPoint = e.changedTouches[0].pageX;
     if (startPoint < endPoint) {
       prevMove();
     } else if (startPoint > endPoint) {
       nextMove();
     }
-  });
+  };
+  // prettier-ignore
+  const cardSlider = () =>
+    _.go(
+      [],
+      () => setWidth(),
+      () => setPagination(),
+      $.findAll('.card-pagination > li'),
+      changeActive,
+      clickPagination,
+      () => $.qs('.cards-section'),
+      _.tap(
+        $.on('touchstart', touchStartEvent)),
+      _.tap(
+        $.on('touchend', touchEndEvent)));
+  return cardSlider;
 };
-
 export default slider;
