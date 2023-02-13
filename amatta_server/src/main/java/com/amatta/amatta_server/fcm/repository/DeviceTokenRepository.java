b7f28eb5b718ac4a9d3993ae07646d5ab56a800b
201724554 <pay7845@naver.com>
2023-02-13 12:22:55 +0900

    feat(BE) : device token CRUD를 위한 repository (#110)

---
+++ amatta_server/src/main/java/com/amatta/amatta_server/fcm/repository/DeviceTokenRepository.java
@@ -0,0 +1,24 @@
+package com.amatta.amatta_server.fcm.repository;
+
+import com.amatta.amatta_server.fcm.model.FCMToken;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DeviceTokenRepository extends CrudRepository<FCMToken, Long> {
+    @Query("SELECT LAST_INSERT_ID()")
+    long last_insert_id();
+
+    @Modifying
+    @Query("INSERT INTO device_token(uid, token) VALUES (:uid, :token)")
+    void addToken(@Param("uid") long uid,
+                  @Param("token") String token);
+
+    @Query("SELECT id, uid, token FROM device_token WHERE uid = :uid")
+    List<FCMToken> findByUid(@Param("uid") long uid);
+}
