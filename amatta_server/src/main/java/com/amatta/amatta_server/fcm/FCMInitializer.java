940afac41cbb75495f0530265975aacc54a73c07
Kimtaewan <65708461+ktykty0722@users.noreply.github.com>
2023-02-13 18:37:57 +0900

    feat(BE) : FCMInitializer 수정 (#125)

--- amatta_server/src/main/java/com/amatta/amatta_server/fcm/FCMInitializer.java
+++ amatta_server/src/main/java/com/amatta/amatta_server/fcm/FCMInitializer.java
@@ -7,27 +7,33 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class FCMInitializer {
-    /*@Value("${firebase.key}")
-    private String firebaseKey;
+    @Value("${firebase.key.path}")
+    private String firebaseKeyPath;
 
     @Value("${firebase.projectId}")
     private String projectId;
 
     @PostConstruct
     private void init() throws IOException {
-        InputStream in = new ByteArrayInputStream(firebaseKey.getBytes());
-
+        InputStream resourceStream = getClass().getResourceAsStream("/team7-amatta-firebase-adminsdk-pa4f5-612a44bb30.json");
+        Path targetPath = Paths.get("team7-amatta-firebase-adminsdk-pa4f5-612a44bb30.json");
+        Files.copy(resourceStream, targetPath, StandardCopyOption.REPLACE_EXISTING);
+        FileInputStream refreshToken = new FileInputStream(targetPath.toString());
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(in))
+                .setCredentials(GoogleCredentials.fromStream(refreshToken))
                 .setProjectId(projectId)
                 .build();
 
         FirebaseApp.initializeApp(options);
-    }*/
+    }
 }
