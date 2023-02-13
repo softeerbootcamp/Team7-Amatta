b3e39ffa404e4c11c50ee6b56cf4c90d479986b8
201724554 <pay7845@naver.com>
2023-02-13 14:27:11 +0900

    refactor(BE) : exception message를 public static final로 선언 (#113)

--- amatta_server/src/main/java/com/amatta/amatta_server/exception/DuplicateGifticonException.java
+++ amatta_server/src/main/java/com/amatta/amatta_server/exception/DuplicateGifticonException.java
@@ -1,4 +1,5 @@
 package com.amatta.amatta_server.exception;
 
 public class DuplicateGifticonException extends RuntimeException {
+    public final static String message = "중복되는 바코드를 가진 기프티콘이 존재합니다";
 }
