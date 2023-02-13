821cd803b7c1ab8ab8b437cacabfb2e3fe8e39fa
Kimtaewan <65708461+ktykty0722@users.noreply.github.com>
2023-02-13 15:01:44 +0900

    BE - UserControllerTest mockMvc 사용으로 수정(#77) (#115)

--- amatta_server/src/test/java/com/amatta/amatta_server/user/UserControllerTest.java
+++ amatta_server/src/test/java/com/amatta/amatta_server/user/UserControllerTest.java
@@ -1,31 +1,29 @@
 package com.amatta.amatta_server.user;
 
 import com.amatta.amatta_server.user.controller.UserController;
+import com.amatta.amatta_server.user.dto.UserChangePasswordReq;
 import com.amatta.amatta_server.user.dto.UserFindPasswordByEmailReq;
 import com.amatta.amatta_server.user.dto.UserJoinReq;
-import com.amatta.amatta_server.user.dto.UserJoinRes;
 import com.amatta.amatta_server.user.dto.UserLoginReq;
-import com.amatta.amatta_server.user.model.Users;
 import com.amatta.amatta_server.user.repository.UserRepository;
 import com.amatta.amatta_server.user.service.UserService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DuplicateKeyException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -47,7 +45,6 @@ public class UserControllerTest {
     @Test
     @DisplayName("정상 회원가입 테스트")
     void join1() throws Exception{
-        //given
         String email = "ktykty0722@naver.com";
         String password = "testPassword";
         String name = "taewan";
@@ -59,25 +56,17 @@ public class UserControllerTest {
                 phoneNumber
         );
 
-        //when
         mockMvc.perform(post("/user/join")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(userJoinReq)))
+                .andExpect(jsonPath("success").value(true))
                 .andExpect(status().isCreated())
                 .andDo(print());
-
-        //then
-        Users joinUser = userRepository.findByEmail(userJoinReq.getEmail());
-        SoftAssertions softAssertions = new SoftAssertions();
-        softAssertions.assertThat(userJoinReq.getName()).isEqualTo(joinUser.getName());
-        softAssertions.assertThat(userJoinReq.getEmail()).isEqualTo(joinUser.getEmail());
-        softAssertions.assertThat(userJoinReq.getPhoneNumber()).isEqualTo(joinUser.getPhoneNumber());
-        softAssertions.assertAll();
     }
 
     @Test
     @DisplayName("회원가입 테스트 - 중복된 이메일 입력")
-    void join2() {
+    void join2() throws Exception {
         UserJoinReq userJoinReq1 = new UserJoinReq(
                 "test@test.com",
                 "testPassword",
@@ -90,18 +79,27 @@ public class UserControllerTest {
                 "test",
                 "010-0000-1111"
         );
-        userController.join(userJoinReq1);
 
-        assertThrows(DuplicateKeyException.class, () -> {
-            userController.join(userJoinReq2);
-        });
+        mockMvc.perform(post("/user/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userJoinReq1)))
+                .andExpect(jsonPath("success").value(true))
+                .andExpect(status().isCreated())
+                .andDo(print());
+
+        mockMvc.perform(post("/user/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userJoinReq2)))
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof DuplicateKeyException))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
     }
 
     @Test
     @DisplayName("회원가입 테스트 - 중복된 전화번호 입력")
-    void join3() {
+    void join3() throws Exception {
         UserJoinReq userJoinReq1 = new UserJoinReq(
-                "test12@test.com",
+                "test1@test.com",
                 "testPassword",
                 "test",
                 "010-0000-0000"
@@ -112,17 +110,77 @@ public class UserControllerTest {
                 "test",
                 "010-0000-0000"
         );
-        userController.join(userJoinReq1);
+        mockMvc.perform(post("/user/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userJoinReq1)))
+                .andExpect(status().isCreated())
+                .andDo(print());
 
-        assertThrows(DuplicateKeyException.class, () -> {
-            userController.join(userJoinReq2);
-        });
+        mockMvc.perform(post("/user/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userJoinReq2)))
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof DuplicateKeyException))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("회원가입 테스트 - 회원가입 시 비밀번호 형식에 맞지 않게 입력했을 경우")
+    void join4() throws Exception {
+        UserJoinReq userJoinReq = new UserJoinReq(
+                "test1@test.com",
+                "1234",
+                "test",
+                "010-0000-0000"
+        );
+
+        mockMvc.perform(post("/user/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userJoinReq)))
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("회원가입 테스트 - 회원가입 시 이메일 형식에 맞지 않게 입력했을 경우")
+    void join5() throws Exception {
+        UserJoinReq userJoinReq = new UserJoinReq(
+                "testtest",
+                "testPassword",
+                "test",
+                "010-0000-0000"
+        );
+
+        mockMvc.perform(post("/user/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userJoinReq)))
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("회원가입 테스트 - 회원가입 시 전화번호 형식에 맞지 않게 입력했을 경우")
+    void join6() throws Exception {
+        UserJoinReq userJoinReq = new UserJoinReq(
+                "test1@test.com",
+                "1234",
+                "test",
+                "01000000000"
+        );
+
+        mockMvc.perform(post("/user/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userJoinReq)))
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
     }
 
     @Test
     @DisplayName("정상 로그인 테스트")
     void login1() throws Exception {
-        //given
         String email = "ktykty0722@naver.com";
         String password = "testPassword";
         String name = "taewan";
@@ -133,77 +191,154 @@ public class UserControllerTest {
                 name,
                 phoneNumber
         );
-        userController.join(userJoinReq);
-        UserLoginReq userLoginReq = new UserLoginReq("ktykty0722@naver.com", "testPassword");
+        mockMvc.perform(post("/user/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userJoinReq)))
+                .andExpect(status().isCreated())
+                .andDo(print());
+        UserLoginReq userLoginReq = new UserLoginReq(email, password);
 
-        //when
         mockMvc.perform(post("/user/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(userLoginReq)))
+                .andExpect(jsonPath("success").value(true))
                 .andExpect(status().isOk())
                 .andDo(print());
-
-        //then
-        Users loginUser = userRepository.findByEmail(userLoginReq.getEmail());
-        SoftAssertions softAssertions = new SoftAssertions();
-        softAssertions.assertThat(userJoinReq.getEmail()).isEqualTo(loginUser.getEmail());
-        softAssertions.assertAll();
     }
 
     @Test
     @DisplayName("로그인 실패 테스트")
-    void login2() {
-        String email = "test@test.com";
+    void login2() throws Exception {
+        String email = "ktykty0722@naver.com";
         String password = "testPassword";
-        String name = "test";
+        String name = "taewan";
         String phoneNumber = "010-0000-0000";
-        UserJoinReq userJoinReq1 = new UserJoinReq(
+        UserJoinReq userJoinReq = new UserJoinReq(
                 email,
                 password,
                 name,
                 phoneNumber
         );
-        userController.join(userJoinReq1);
-
+        mockMvc.perform(post("/user/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userJoinReq)))
+                .andExpect(status().isCreated())
+                .andDo(print());
         UserLoginReq userLoginReq = new UserLoginReq(email, password + 1);
 
-        Users user = userService.login(userLoginReq);
-        assertNull(user);
+        mockMvc.perform(post("/user/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userLoginReq)))
+                .andExpect(jsonPath("success").value(false))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
     }
 
     @Test
     @DisplayName("비밀번호 이메일로 찾기 테스트")
     void findPassword1() throws Exception {
-        //given
         String email = "ktykty0722@naver.com";
         String password = "testPassword";
         String name = "taewan";
         String phoneNumber = "010-0000-0000";
-        UserJoinReq userJoinReq1 = new UserJoinReq(
+        UserJoinReq userJoinReq = new UserJoinReq(
                 email,
                 password,
                 name,
                 phoneNumber
         );
-        userController.join(userJoinReq1);
-        UserFindPasswordByEmailReq userFindPasswordByEmailReq = new UserFindPasswordByEmailReq("taewan", "ktykty0722@naver.com");
-        Users BeforeChangePassword = userRepository.findByEmail(userFindPasswordByEmailReq.getEmail());
+        mockMvc.perform(post("/user/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userJoinReq)))
+                .andExpect(status().isCreated())
+                .andDo(print());
+        UserFindPasswordByEmailReq userFindPasswordByEmailReq = new UserFindPasswordByEmailReq(name, email);
 
-        //when
         mockMvc.perform(post("/user/find/password/email")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(userFindPasswordByEmailReq)))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("success").value(true))
                 .andDo(print());
-
-        //then
-        Users AfterChangePassword = userRepository.findByEmail(userFindPasswordByEmailReq.getEmail());
-        SoftAssertions softAssertions = new SoftAssertions();
-        softAssertions.assertThat(BeforeChangePassword.getName()).isEqualTo(AfterChangePassword.getName());
-        softAssertions.assertThat(BeforeChangePassword.getEmail()).isEqualTo(AfterChangePassword.getEmail());
-        softAssertions.assertThat(BeforeChangePassword.getPhoneNumber()).isEqualTo(AfterChangePassword.getPhoneNumber());
-        softAssertions.assertThat(BeforeChangePassword.getPassword()).isNotEqualTo(AfterChangePassword.getPassword());
-        softAssertions.assertAll();
     }
 
+    @Test
+    @DisplayName("마이페이지 테스트")
+    void myPage1() throws Exception {
+        String email = "ktykty0722@naver.com";
+        String password = "testPassword";
+        String name = "taewan";
+        String phoneNumber = "010-0000-0000";
+        UserJoinReq userJoinReq = new UserJoinReq(
+                email,
+                password,
+                name,
+                phoneNumber
+        );
+        mockMvc.perform(post("/user/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userJoinReq)))
+                .andExpect(status().isCreated())
+                .andDo(print());
+
+        UserLoginReq userLoginReq = new UserLoginReq(email, password);
+        MockHttpSession httpSession = new MockHttpSession();
+        mockMvc.perform(post("/user/login")
+                        .session(httpSession)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userLoginReq)))
+                .andExpect(jsonPath("success").value(true))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        mockMvc.perform(get("/user/mypage")
+                        .session(httpSession))
+                .andExpect(jsonPath("email").value(email))
+                .andExpect(jsonPath("name").value(name))
+                .andExpect(jsonPath("phonenumber").value(phoneNumber))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+    }
+
+    @Test
+    @DisplayName("비밀번호 변경 테스트")
+    void changePassword() throws Exception {
+        String email = "ktykty0722@naver.com";
+        String password = "testPassword";
+        String name = "taewan";
+        String phoneNumber = "010-0000-0000";
+        UserJoinReq userJoinReq = new UserJoinReq(
+                email,
+                password,
+                name,
+                phoneNumber
+        );
+        mockMvc.perform(post("/user/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userJoinReq)))
+                .andExpect(status().isCreated())
+                .andDo(print());
+
+        UserLoginReq userLoginReq = new UserLoginReq(email, password);
+        MockHttpSession httpSession = new MockHttpSession();
+        mockMvc.perform(post("/user/login")
+                        .session(httpSession)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userLoginReq)))
+                .andExpect(jsonPath("success").value(true))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+
+        UserChangePasswordReq userChangePasswordReq = new UserChangePasswordReq("changePassword");
+        mockMvc.perform(put("/user/password")
+                        .session(httpSession)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userChangePasswordReq)))
+                .andExpect(jsonPath("success").value(true))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+    }
 }
