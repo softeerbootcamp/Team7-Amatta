674fb957fd7195f6b60b5867240c92210838a22b
201724554 <pay7845@naver.com>
2023-02-16 21:34:38 +0900

    feat(BE) : 스케줄러를 이용해 만료가 가까운 기프티콘 관련 알림 전송 기능 구현 (#142)

--- amatta_server/src/main/java/com/amatta/amatta_server/fcm/service/FCMService.java
+++ amatta_server/src/main/java/com/amatta/amatta_server/fcm/service/FCMService.java
@@ -2,18 +2,21 @@ package com.amatta.amatta_server.fcm.service;
 
 import com.amatta.amatta_server.aop.MethodRequiresAuth;
 import com.amatta.amatta_server.fcm.dto.TokenRegisterDto;
-import com.amatta.amatta_server.fcm.model.FCMToken;
 import com.amatta.amatta_server.fcm.repository.DeviceTokenRepository;
+import com.amatta.amatta_server.gifticon.model.Gifticon;
 import com.amatta.amatta_server.user.model.Users;
 import com.google.firebase.messaging.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -32,41 +35,37 @@ public class FCMService {
         tokenRepository.addToken(user.getId(), tokenRegisterDto.getToken());
     }
 
-    public void sendPushNotification(long uid) {
-        for(FCMToken token : tokenRepository.findByUid(uid)) {
-            Message message = makeExpirationMessage(token);
-            try {
-                FirebaseMessaging.getInstance().send(message);
-            } catch (FirebaseMessagingException e) {
-                e.printStackTrace();
-            }
+    private void sendMessage(List<String> tokens, String title, String body) throws FirebaseMessagingException {
+        List<Message> messages = new ArrayList<>();
+        for(String token : tokens) {
+            messages.add(generateMessages(token, title, body));
         }
+        FirebaseMessaging.getInstance().sendAll(messages);
     }
 
-    private Message makeExpirationMessage(FCMToken targetToken) {
+    private void sendMessage(String tokens, String title, String body) throws FirebaseMessagingException {
+        Message messages = generateMessages(tokens, title, body);
+        FirebaseMessaging.getInstance().send(messages);
+    }
+
+    private Message generateMessages(String token, String title, String body) {
         return Message.builder()
-                .setToken(targetToken.getToken())
-                .setNotification(Notification.builder()
-                        .setTitle("title")
-                        .setBody("body")
+                .setToken(token)
+                .setWebpushConfig(WebpushConfig.builder()
+                        .setNotification(WebpushNotification.builder()
+                                .setTitle(title)
+                                .setBody(body)
+                                .setIcon("icon")
+                                .setVibrate(new int[]{200, 100, 200, 100, 200, 100, 200})
+                                .build())
                         .build())
                 .build();
-    }
-
-    private Users getUserBySessionId() {
-        HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
-        HttpSession session = request.getSession(false);
-        return (Users) session.getAttribute("User");
     }
 
     public void sendTestMessage(String token) throws FirebaseMessagingException {
         System.out.println(token);
         Message message = Message.builder()
                 .setToken(token)
-                /*.setNotification(Notification.builder()
-                        .setTitle("notification title")
-                        .setBody("notification body")
-                        .build())*/
                 .setWebpushConfig(WebpushConfig.builder()
                         .setNotification(WebpushNotification.builder()
                                 .setBody("push body")
@@ -78,26 +77,24 @@ public class FCMService {
         FirebaseMessaging.getInstance().send(message);
     }
 
-    @Scheduled(cron = "*/10 * * * * *")
-    public void sendTestMessageToAllTokens() {
-        System.out.println("스케줄러");
-        List<FCMToken> list = tokenRepository.findAllTokens();
-        for(FCMToken token : list) {
-            Message message = Message.builder()
-                    .setToken(token.getToken())
-                    .setWebpushConfig(WebpushConfig.builder()
-                            .setNotification(WebpushNotification.builder()
-                                    .setBody("push body")
-                                    .setTitle("push title")
-                                    .setVibrate(new int[]{200, 100, 200, 100, 200, 100, 200})
-                                    .build())
-                            .build())
-                    .build();
+    @Scheduled(cron = "*/20 * * * * *")
+    @Transactional(readOnly = true)
+    public void scheduleExpirationAlarmMessage() {
+        System.out.println("expire");
+        LocalDate now = LocalDate.now();
+        List<String> tokens = tokenRepository.findTokensByUidsOfGifticonsAboutToExpire(now.plusDays(Gifticon.expirationThresholdDays));
+        if (!tokens.isEmpty()){
             try {
-                FirebaseMessaging.getInstance().send(message);
+                sendMessage(tokens, "A! matta", "거의 만료된 기프티콘이 있어요");
             } catch (FirebaseMessagingException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private Users getUserBySessionId() {
+        HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+        HttpSession session = request.getSession(false);
+        return (Users) session.getAttribute("User");
     }
 }
