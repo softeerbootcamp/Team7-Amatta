f82f1b23974ce3ff03eb9152e9b474d56334592a
201724554 <pay7845@naver.com>
2023-02-18 01:33:15 +0900

    feat(FE) : 백그라운드에서 메세지 수신 시 실행할 이벤트 추가 (#161)

---
+++ client/firebase-messaging-sw.js
@@ -0,0 +1,17 @@
+importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-app-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCsLBsvozvTnYlDH-5cS0A8X_AjV5o4jjM",
+  authDomain: "amatta-4934f.firebaseapp.com",
+  projectId: "amatta-4934f",
+  storageBucket: "amatta-4934f.appspot.com",
+  messagingSenderId: "196308516589",
+  appId: "1:196308516589:web:64545440aa5021e8a496e4",
+  measurementId: "G-4JBCQPF50K"
+});
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload)=>{
+  console.log("background received", payload);
+});
