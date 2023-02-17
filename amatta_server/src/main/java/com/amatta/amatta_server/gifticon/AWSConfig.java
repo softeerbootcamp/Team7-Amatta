b0a1578d84f413cb53351c05d097f9041f1a5432
ktykty0722 <ktykty0722@naver.com>
2023-02-17 21:49:41 +0900

    feat(BE) : s3에 파일 업로드를 위한 설정파일 추가(#159)

---
+++ amatta_server/src/main/java/com/amatta/amatta_server/gifticon/AWSConfig.java
@@ -0,0 +1,29 @@
+package com.amatta.amatta_server.gifticon;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSConfig {
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String iamAccessKey;
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String iamSecretKey;
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(iamAccessKey, iamSecretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+                .build();
+    }
+}
