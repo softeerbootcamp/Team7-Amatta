b6a3483e13bd31ecee3e631ccef492390ea29070
201724554 <pay7845@naver.com>
2023-02-21 10:33:48 +0900

    feat(BE) : TokenRegisterDto에서 token 유효성 체크 (#167)

--- amatta_server/src/main/java/com/amatta/amatta_server/fcm/dto/TokenRegisterDto.java
+++ amatta_server/src/main/java/com/amatta/amatta_server/fcm/dto/TokenRegisterDto.java
@@ -6,10 +6,12 @@ import javax.validation.constraints.NotEmpty;
 
 @Getter
 public class TokenRegisterDto {
-    @NotEmpty
-    private final String token;
+    @NotEmpty(message = "토큰이 존재해야 합니다")
+    private String token;
 
     public TokenRegisterDto(String token) {
         this.token = token;
     }
+
+    public TokenRegisterDto(){}
 }
