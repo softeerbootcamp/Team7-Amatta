3b8d2266ccd60737bdb7fcd1dd92ca46245c83d9
sunjh96 <sunjh96@gmail.com>
2023-02-22 18:35:44 +0900

    feat(FE) : PWA 등록 기능 추가(#164)

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 export default defineConfig({
@@ -7,11 +8,59 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
+    assetsDir: '.',
+    assetsInclude: ['/*.js', '/.css', '**/.html', '**/.json', 'service-worker.js'],
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
-        firebase: path.resolve(__dirname, 'firebase-messaging-sw.js'),
+        offline: path.resolve(__dirname, 'offline.html'),
+        'service-worker': path.resolve(__dirname, 'service-worker.js'),
       },
     },
   },
+  plugins: [
+    VitePWA({
+      mode: 'production',
+      filename: 'service-worker.js', // 변경된 파일 이름
+      base: '/',
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'assets/'],
+      manifest: {
+        name: '아 맞다!',
+        short_name: 'Amatta',
+        icons: [
+          {
+            src: 'icon-x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: 'icon-x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        start_url: 'https://amatta.site/', // 변경된 start_url
+        display: 'fullscreen',
+        background_color: '#92b8b1',
+        theme_color: '#ffffff',
+      },
+      workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
+        swDest: 'dist/service-worker.js', // 변경된 파일 경로
+      },
+    }),
+  ],
 });
