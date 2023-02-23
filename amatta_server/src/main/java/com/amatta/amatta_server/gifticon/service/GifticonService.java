b6ce22a590b3b47a7aafc82268afeafb176e723e
ktykty0722 <ktykty0722@naver.com>
2023-02-23 20:22:56 +0900

    fix(BE) : 기프티콘 서비스 AOP 관련 어노테이션 주석 해제

--- amatta_server/src/main/java/com/amatta/amatta_server/gifticon/service/GifticonService.java
+++ amatta_server/src/main/java/com/amatta/amatta_server/gifticon/service/GifticonService.java
@@ -36,7 +36,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Service
-//@ClassRequiresAuth
+@ClassRequiresAuth
 public class GifticonService {
     private final GifticonRepository gifticonRepository;
     private final RequestGenerator requestGenerator;
